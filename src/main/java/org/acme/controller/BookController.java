04dfefbd43f8e940eecd78436724d402cd176338
mokhirDev <moxirbek.maxkamov021199@gmail.com>
2024-11-22 19:06:59 +0500

    created keycloak

--- src/main/java/org/acme/controller/BookController.java
+++ src/main/java/org/acme/controller/BookController.java
@@ -1,40 +1,56 @@
 package org.acme.controller;
 
+import io.quarkus.security.Authenticated;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.acme.base.BaseController;
-import org.acme.base.BaseService;
 import org.acme.dto.book.BookRequestDTO;
 import org.acme.dto.book.BookResponseDTO;
-import org.acme.dto.user.UserResponseDTO;
-import org.acme.entity.Book;
 import org.acme.service.BookService;
+import java.util.List;
 
 @Path("/api/books")
 @ApplicationScoped
-public class BookController extends BaseController<BookRequestDTO, BookResponseDTO, Long, UserResponseDTO> {
+@Authenticated
+public class BookController{
 
     @Inject
     BookService bookService;
 
-    @Override
-    protected BaseService<BookRequestDTO, BookResponseDTO, Book, Long, UserResponseDTO> getService() {
-        return bookService;
-    }
-
     @GET
     @Path("/find/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"admin"})
+    @RolesAllowed({"admin", "author", "user"})
     public Response findBookById(@PathParam("id") Long id) {
 //        return Response.ok(bookService.findByIdWithRelatedEntities(id)).build();
         return Response.ok(bookService.findById(id)).build();
+    }
+
+    @POST
+    @Path("/add")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("author")
+    public Response create(BookRequestDTO dto) {
+        return Response.ok(bookService.save(dto)).build();
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    @RolesAllowed("author")
+    public Response delete(@PathParam("id") Long id) {
+        bookService.delete(id);
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Path("/all")
+    @RolesAllowed({"admin", "author"})
+    public Response findAll() {
+        List<BookResponseDTO> all = bookService.findAll();
+        return Response.ok(all).build();
     }
 }
--- src/main/java/org/acme/controller/UserController.java
+++ src/main/java/org/acme/controller/UserController.java
@@ -1,40 +1,80 @@
 package org.acme.controller;
 
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.acme.base.BaseController;
-import org.acme.base.BaseService;
-import org.acme.dto.book.BookResponseDTO;
 import org.acme.dto.user.UserRequestDTO;
 import org.acme.dto.user.UserResponseDTO;
-import org.acme.entity.User;
 import org.acme.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Path("/api/users")
 @ApplicationScoped
-public class UserController extends BaseController<UserRequestDTO, UserResponseDTO, Long, BookResponseDTO> {
+@Authenticated
+public class UserController {
 
     @Inject
     UserService userService;
+    @Inject
+    SecurityIdentity securityIdentity;
 
-    @Override
-    protected BaseService<UserRequestDTO, UserResponseDTO, User, Long, BookResponseDTO> getService() {
-        return userService;
+    @GET
+    @Path("/info")
+    public Response getUserInfo() {
+        String username = securityIdentity.getPrincipal().getName();
+        boolean isReader = securityIdentity.hasRole("reader");
+        return Response.ok("Username: " + username + ", isReader: " + isReader).build();
     }
+
+//    @GET
+//    @Path("/get/{id}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @RolesAllowed("user")
+//    public Response findUserById(@PathParam("id") Long id) {
+//        return Response.ok(userService.findByIdWithRelatedEntities(id)).build();
+//        return Response.ok(userService.findById(id)).build();
+//    }
 
     @GET
     @Path("/get/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"user", "admin", "user"})
+    public Response findById(@PathParam("id") Long id) {
+        Optional<UserResponseDTO> entity = userService.findById(id);
+        return entity.map(Response::ok)
+                .orElse(Response.status(Response.Status.NOT_FOUND))
+                .build();
+    }
+
+    @Path("/add")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("admin")
-    public Response findUserById(@PathParam("id") Long id) {
-//        return Response.ok(userService.findByIdWithRelatedEntities(id)).build();
-        return Response.ok(userService.findById(id)).build();
+    public Response addUser(UserRequestDTO dto) {
+        return Response.ok(userService.save(dto)).build();
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    @RolesAllowed("admin")
+    public Response delete(@PathParam("id") Long id) {
+        userService.delete(id);
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Path("/all")
+    @RolesAllowed("admin")
+    public Response findAll() {
+        List<UserResponseDTO> all = userService.findAll();
+        return Response.ok(all).build();
     }
 }
